a015be2f90e852b951f7a4bf7e07ebad6a060f11
SAFA IDAM HAMED <safa.idamhamed2@gmail.com>
2024-01-31 13:46:02 +0100

    line block

--- front-end/src/styles.js
+++ front-end/src/styles.js
@@ -1,7 +1,7 @@
 const styles = {
- hoverli: 'sm:hover:text-[#614d76] sm:hover:ease-out duration-300 sm:ease-out duration-300 sm:hover:scale-95',
- line2position: 'sm:justify-center sm:w-[1200px] sm:ml-auto sm:mr-auto mt-6 mr-auto ml-auto text-center w-[290px] ',
- buttonDesign: 'b'
+  hoverli: 'sm:hover:text-[#614d76] sm:hover:ease-out duration-300 sm:ease-out duration-300 sm:hover:scale-95',
+  line2position: 'sm:justify-center sm:w-[1200px] sm:ml-auto sm:mr-auto mt-6 mr-auto ml-auto text-center w-[290px]',
+  buttonDesign: 'b'
 };
 
 
